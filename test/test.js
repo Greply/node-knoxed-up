767bc756c0944bd0cf1629d15ca57a64f5a6a718
Mark Armendariz <src@enobrev.com>
2012-10-16 12:28:40 -0700

    added nodeunit and tests for download to temp as well as updating headers

---
+++ test/test.js
@@ -0,0 +1,52 @@
+    var KnoxedUp = require("../knoxed-up");
+    var fs       = require('fs');
+    var testCase = require('nodeunit').testCase;
+    var syslog   = require('syslog-console').init('KnoxedUp');
+
+    syslog.disableTTY();
+
+    // CREATE INSTANCE
+
+    var S3 = new KnoxedUp({
+        key:    'AKIAJ7CBLVZ2DSXOOBWQ',
+        secret: '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',
+        bucket: 'media.cameoapp.com'
+    });
+
+    var sFileHash = '62228dc488ce4a2619e460c117254db404981b1e';
+    var aPath     = sFileHash.split('').slice(0, 3);
+    var sPath     = aPath.join('/') + '/' + sFileHash;
+
+    exports["Test Download To Temp"] = {
+        "To Temp": function(test) {
+            test.expect(4);
+            test.ok(S3 instanceof KnoxedUp, "Instance created");
+
+            S3.toTemp(sPath, 'binary', '.avi', function(sTempFile, sHash) {
+                test.equal(sHash,     sFileHash,          "Downloaded file has Correct Hash");
+                test.equal(sTempFile, '/tmp/' + sFileHash + '.avi', "Temp file is Named Correctly");
+
+                fs.exists(sTempFile, function(bExists) {
+                    test.ok(bExists, "File Exists in /tmp");
+                    test.done();
+                });
+            });
+        }
+    };
+
+    exports["Update Headers"] = {
+        "To Temp": function(test) {
+            test.expect(1);
+
+            var oHeaders = {
+                'Content-Type': 'video/vnd.avi'
+            };
+
+            S3.updateHeaders('5/9/3/593949e21dee8eeb9c7af2f26b87f8bb0c2241c3', oHeaders, function(oUpdateError) {
+                S3.getHeaders('5/9/3/593949e21dee8eeb9c7af2f26b87f8bb0c2241c3', function(oGetError, oGetHeaders) {
+                    test.equal(oGetHeaders['content-type'], oHeaders['Content-Type'], 'Headers Updated Correctly');
+                    test.done();
+                });
+            });
+        }
+    };
